b14bfa7fe61978ff5f415efd866bc567bad839bf
enhuili <lexi05732@gmail.com>
2024-02-20 11:30:13 -0500

    Wrote equivalence test for getWordSet/Dictionary

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -29,4 +29,69 @@ class DictionaryTest {
         var wordSet = dictionary.getWordSet();
         assertTrue(wordSet.isEmpty());
     }
+    @Test
+    void caseInsensitive(){
+        var dictionary = new Dictionary();
+
+        var wordSet = dictionary.getWordSet();
+        wordSet.add("APPLE");
+        assertTrue(wordSet.contains("apple"));
+    }
+
+    @Test
+    void IllegalWords(){
+        var dictionary = new Dictionary();
+
+        var wordSet = dictionary.getWordSet();
+        wordSet.add("wocao");
+        assertTrue(wordSet.isEmpty());
+    }
+
+    @Test
+    void checkLowercase(){
+        var dictionary = new Dictionary();
+        dictionary.addWord("APPLE");
+        dictionary.addWord("BANANA");
+        dictionary.addWord("PINEAPPLE");
+
+        var wordSet = dictionary.getWordSet();
+        for(String s : wordSet){
+            String wocao = s.toLowerCase();
+            assertTrue(wocao.equals(s));
+        }
+        Set<String> result = dictionary.getWordSet();
+    }
+
+//    @Test
+//    void equalWords(){
+//        var dictionary = new Dictionary();
+//        var wordSet = dictionary.getWordSet();
+//
+//        var dictionary1 = new Dictionary();
+//        var wordSet1 = dictionary1.getWordSet();
+//
+//        wordSet.add("apple");
+//        wordSet1.add("apple");
+//        assertTrue(wordSet.equals(wordSet1));
+//    }
+
+    @Test
+    void sizeCheck(){
+        var dictionary = new Dictionary();
+        var wordSet = dictionary.getWordSet();
+        wordSet.add("apple");
+        wordSet.add("banana");
+        wordSet.add("pineapple");
+        int size = wordSet.size();
+        assertTrue(size == 3);
+    }
+
+    @Test
+    void checkEmpty(){
+        var dictionary = new Dictionary();
+        var wordSet = dictionary.getWordSet();
+        wordSet.add("apple");
+        assertFalse(wordSet.isEmpty());
+//        assertTrue(!wordSet.isEmpty());
+    }
 }
