bddd6fc219cff450184b54f907addc4357ca5a02
enhuili <lexi05732@gmail.com>
2024-02-21 15:11:11 -0500

    Deleted unnecessary codes in DictionaryTest

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -28,7 +28,6 @@ class DictionaryTest {
     @Test
     void getWordSet_initiallyEmpty() {
         var dictionary = new Dictionary();
-
         var wordSet = dictionary.getWordSet();
         assertTrue(wordSet.isEmpty());
     }
@@ -56,17 +55,13 @@ class DictionaryTest {
     @Test
     void checkSize(){
         var dictionary = new Dictionary();
-        var wordSet = dictionary.getWordSet();
         assertEquals(0, dictionary.size());
 
 
         var dictionary1 = new Dictionary();
-
         dictionary1.addWord("apple");
         dictionary1.addWord("black");
         dictionary1.addWord("camel");
-
-        var wordSet1 = dictionary.getWordSet();
         assertEquals(3, dictionary1.size());
     }
 
@@ -76,7 +71,6 @@ class DictionaryTest {
         dictionary.addWord("apple");
         dictionary.addWord("BlAcK");
         dictionary.addWord("CAMEL");
-
 
         assertTrue(dictionary.contains("apple"));
         assertTrue(dictionary.contains("black"));
