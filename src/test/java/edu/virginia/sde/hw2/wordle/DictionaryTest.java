feb0cd50ad58862c2bf45ba026f7245262a37acb
enhuili <lexi05732@gmail.com>
2024-02-21 14:32:04 -0500

    Modified size/contains/addWords those three tests in Dictionary Test

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -32,78 +32,56 @@ class DictionaryTest {
         var wordSet = dictionary.getWordSet();
         assertTrue(wordSet.isEmpty());
     }
-//
-//    @Test
-//    void checkValidateWordSet(){
-////        var startingWordSet = new HashSet<>(Set.of("", "Status"));
-//        var dictionary = new Dictionary();
-//        dictionary.addWord("");
-//        dictionary.addWord("black");
-//        var wordSet = dictionary.getWordSet();
-//        for(String s : wordSet){
-//            String temp = s.toLowerCase();
-//            assertTrue(temp.equals(s));
-//        }
-//    }
 
     @Test
     void checkContains(){
         var dictionary = new Dictionary();
 
-        assertTrue(!dictionary.contains("apple"));
-        assertTrue(!dictionary.contains("black"));
-        assertTrue(!dictionary.contains("camel"));
+        assertFalse(dictionary.contains("apple"));
+        assertFalse(dictionary.contains("black"));
+        assertFalse(dictionary.contains("camel"));
 
         dictionary.addWord("apple");
         dictionary.addWord("black");
+        dictionary.addWord("APPLE");
 
         assertTrue(dictionary.contains("apple"));
         assertTrue(dictionary.contains("black"));
-        assertTrue(!dictionary.contains("camel"));
-    }
+        assertFalse(dictionary.contains("camel"));
 
-//    @Test
-//    void checkIsEmpty(){
-//        var dictionary = new Dictionary();
-//        assertTrue(dictionary.isEmpty());
-//
-//        dictionary.addWord("apple");
-//        assertTrue(!dictionary.isEmpty());
-//    }
+    }
 
     @Test
     void checkSize(){
         var dictionary = new Dictionary();
+
         assertTrue(dictionary.size() == 0);
 
         dictionary.addWord("apple");
+        dictionary.addWord("black");
+        dictionary.addWord("camel");
+        dictionary.addWord("!pple");
         dictionary.addWord("banana");
-        dictionary.addWord("pineapple");
-        dictionary.addWord("!apple");
 
         assertTrue(dictionary.size() == 3);
     }
-//
-//    @Test
-//    void checkEquals(){
-//        var startingWordSet = new HashSet<>(Set.of(""));
-//        var dictionary = new Dictionary(startingWordSet, new WordValidator());
-//
-//        assertFalse(dictionary.equals(""));
-//
-//        var dictionary1 = new Dictionary();
-//        dictionary1.addWord("");
-//
-//        assertTrue(dictionary.equals(dictionary1));
-//    }
 
     @Test
     void checkAddWord(){
         var dictionary = new Dictionary();
         dictionary.addWord("");
         dictionary.addWord("apple");
-        assertTrue(dictionary.contains("apple"));
+        dictionary.addWord("black");
+        dictionary.addWord("camel");
+        dictionary.addWord("banana");
+        dictionary.addWord("!appl");
+
         assertFalse(dictionary.contains(""));
+        assertTrue(dictionary.contains("apple"));
+        assertTrue(dictionary.contains("black"));
+        assertTrue(dictionary.contains("CAMEL"));
+        assertFalse(dictionary.contains("banana"));
+        assertFalse(dictionary.contains("!appl"));
 
     }
 }
