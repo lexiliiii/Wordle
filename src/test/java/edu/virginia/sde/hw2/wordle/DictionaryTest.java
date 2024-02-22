a449bd5a47060e5d47b56ba4e59d08ed74a521c0
enhuili <lexi05732@gmail.com>
2024-02-22 16:28:07 -0500

    Wrote equivalence test in DictionaryTest and WordValidatorTest

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -1,9 +1,12 @@
 package edu.virginia.sde.hw2.wordle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.beans.BeanProperty;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,8 +28,66 @@ class DictionaryTest {
     @Test
     void getWordSet_initiallyEmpty() {
         var dictionary = new Dictionary();
-
         var wordSet = dictionary.getWordSet();
         assertTrue(wordSet.isEmpty());
+    }
+
+    @Test
+    void checkContains(){
+        var dictionary = new Dictionary();
+
+        assertFalse(dictionary.contains("apple"));
+        assertFalse(dictionary.contains("black"));
+        assertFalse(dictionary.contains("camel"));
+
+        dictionary.addWord("apple");
+        dictionary.addWord("black");
+        dictionary.addWord("CAMEL");
+        dictionary.addWord("teach");
+
+        assertTrue(dictionary.contains("apple"));
+        assertTrue(dictionary.contains("black"));
+        assertTrue(dictionary.contains("camel"));
+        assertTrue(dictionary.contains("TEACH"));
+
+    }
+
+    @Test
+    void checkSize(){
+        var dictionary = new Dictionary();
+        assertEquals(0, dictionary.size());
+
+        var dictionary1 = new Dictionary();
+
+        dictionary1.addWord("apple");
+        dictionary1.addWord("black");
+        dictionary1.addWord("camel");
+
+        assertTrue(dictionary1.size() == 3);
+    }
+
+    @Test
+    void checkAddWord(){
+        var dictionary = new Dictionary();
+        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord(""));
+        dictionary.addWord("apple");
+        dictionary.addWord("black");
+        dictionary.addWord("camel");
+        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("banana"));
+        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("a@ple"));
+        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("a!ple"));
+        dictionary.addWord("WHITE");
+        dictionary.addWord("TeaCh");
+
+
+        assertFalse(dictionary.contains(""));
+        assertTrue(dictionary.contains("apple"));
+        assertTrue(dictionary.contains("black"));
+        assertTrue(dictionary.contains("CAMEL"));
+        assertFalse(dictionary.contains("banana"));
+        assertFalse(dictionary.contains("!appl"));
+        assertTrue(dictionary.contains("white"));
+        assertTrue(dictionary.contains("teach"));
+
     }
 }
--- src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
@@ -13,13 +13,26 @@ public class WordValidatorTest {
     }
     @Test
     public void isValidWord_false_tooLong() {
+
+        assertTrue(wordValidator.isValidWord("apple"));
+        assertTrue(wordValidator.isValidWord("BLACK"));
+        assertTrue(wordValidator.isValidWord("Camel"));
         assertFalse(wordValidator.isValidWord("DOGGIE"));
+        assertFalse(wordValidator.isValidWord("banana"));
+        assertFalse(wordValidator.isValidWord("dog"));
+
     }
 
     @Test
-    public void getLetterResultTest() {
+    public void checkIsAllLetters(){
+        assertTrue(wordValidator.isAllLetters("apple"));
+        assertTrue(wordValidator.isAllLetters("APPLE"));
+        assertTrue(wordValidator.isAllLetters("Apple"));
+        assertTrue(wordValidator.isAllLetters("ApPlE"));
+        assertFalse(wordValidator.isAllLetters(""));
+        assertTrue(wordValidator.isAllLetters("banana"));
+        assertFalse(wordValidator.isAllLetters("a1ppl"));
+        assertFalse(wordValidator.isAllLetters("a@ppl"));
 
     }
-
-
 }
