075e8af41f22a1b8bcd9737bf692cfbd0047998f
enhuili <lexi05732@gmail.com>
2024-02-23 16:31:20 -0500

    Wrote equivalence test of Contains(empty dictionary should not contain any words/dictionary should contain the valid word we have added/cases of UpperCase) in DictionaryTest

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -33,61 +33,42 @@ class DictionaryTest {
     }
 
     @Test
-    void checkContains(){
+    void checkNotContains(){
         var dictionary = new Dictionary();
 
         assertFalse(dictionary.contains("apple"));
-        assertFalse(dictionary.contains("black"));
-        assertFalse(dictionary.contains("camel"));
+
+    }
+
+    @Test
+    void checkContains(){
+        var dictionary = new Dictionary();
 
         dictionary.addWord("apple");
-        dictionary.addWord("black");
-        dictionary.addWord("CAMEL");
-        dictionary.addWord("teach");
 
         assertTrue(dictionary.contains("apple"));
-        assertTrue(dictionary.contains("black"));
-        assertTrue(dictionary.contains("camel"));
+
+    }
+
+    @Test
+    void checkContainsUppercase(){
+        var dictionary = new Dictionary();
+
+        dictionary.addWord("teach");
+
         assertTrue(dictionary.contains("TEACH"));
 
     }
 
     @Test
-    void checkSize(){
+    void checkContainsUppercase1(){
         var dictionary = new Dictionary();
-        assertEquals(0, dictionary.size());
 
-        var dictionary1 = new Dictionary();
-
-        dictionary1.addWord("apple");
-        dictionary1.addWord("black");
-        dictionary1.addWord("camel");
-
-        assertTrue(dictionary1.size() == 3);
-    }
-
-    @Test
-    void checkAddWord(){
-        var dictionary = new Dictionary();
-        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord(""));
-        dictionary.addWord("apple");
-        dictionary.addWord("black");
-        dictionary.addWord("camel");
-        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("banana"));
-        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("a@ple"));
-        assertThrows(IllegalArgumentException.class, () -> dictionary.addWord("a!ple"));
         dictionary.addWord("WHITE");
-        dictionary.addWord("TeaCh");
 
-
-        assertFalse(dictionary.contains(""));
-        assertTrue(dictionary.contains("apple"));
-        assertTrue(dictionary.contains("black"));
-        assertTrue(dictionary.contains("CAMEL"));
-        assertFalse(dictionary.contains("banana"));
-        assertFalse(dictionary.contains("!appl"));
         assertTrue(dictionary.contains("white"));
-        assertTrue(dictionary.contains("teach"));
 
     }
+
+
 }
