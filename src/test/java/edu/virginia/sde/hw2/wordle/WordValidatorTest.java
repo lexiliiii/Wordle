2c8ef210a609e5b39ae47ce499241f8d8e3257f9
enhuili <lexi05732@gmail.com>
2024-02-24 17:42:25 -0500

    Wrote equivalence test for IsAllLetters(valid word/upper case/empty string/contains number/contains symbol

--- src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WordValidatorTest {
     private static WordValidator wordValidator;
-
     @BeforeAll
     public static void initialize() {
         wordValidator = new WordValidator();
@@ -17,18 +16,10 @@ public class WordValidatorTest {
 
         assertFalse(wordValidator.isValidWord("DOGGIE"));
 
-        assertTrue(wordValidator.isValidWord("apple"));
-        assertTrue(wordValidator.isValidWord("BLACK"));
-        assertTrue(wordValidator.isValidWord("Camel"));
-        assertFalse(wordValidator.isValidWord("DOGGIE"));
-        assertFalse(wordValidator.isValidWord("banana"));
-        assertFalse(wordValidator.isValidWord("dog"));
-
-
     }
 
     @Test
-    public void checkIsAllLetters(){
+    public void validWordIsAllLetters(){
         assertTrue(wordValidator.isAllLetters("apple"));
 
     }
@@ -36,30 +27,27 @@ public class WordValidatorTest {
     @Test
     public void UpperCaseIsAllLetters(){
         assertTrue(wordValidator.isAllLetters("APPLE"));
+
     }
 
 
     @Test
     public void EmptyStringIsAllLetters(){
         assertFalse(wordValidator.isAllLetters(""));
+
     }
 
 
     @Test
     public void containsNumberIsAllLetters(){
         assertFalse(wordValidator.isAllLetters("a1ppl"));
+
     }
+
 
     @Test
     public void containsSymbolIsAllLetters(){
-
-        assertTrue(wordValidator.isAllLetters("APPLE"));
-        assertTrue(wordValidator.isAllLetters("Apple"));
-        assertTrue(wordValidator.isAllLetters("ApPlE"));
-        assertFalse(wordValidator.isAllLetters(""));
-        assertTrue(wordValidator.isAllLetters("banana"));
-        assertFalse(wordValidator.isAllLetters("a1ppl"));
-
         assertFalse(wordValidator.isAllLetters("a@ppl"));
+
     }
 }
