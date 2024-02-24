4e8c18f4043b2b329a2e46b2fa50994415f5bf9b
enhuili <lexi05732@gmail.com>
2024-02-24 17:19:25 -0500

    Wrote equivalence test for IsAllLetters(valid word/upper case/empty string/contains number/contains symbol

--- src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
@@ -7,46 +7,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WordValidatorTest {
     private static WordValidator wordValidator;
+
     @BeforeAll
     public static void initialize() {
         wordValidator = new WordValidator();
     }
     @Test
     public void isValidWord_false_tooLong() {
-
         assertFalse(wordValidator.isValidWord("DOGGIE"));
-
     }
 
     @Test
     public void validWordIsAllLetters(){
         assertTrue(wordValidator.isAllLetters("apple"));
-
     }
 
     @Test
     public void UpperCaseIsAllLetters(){
         assertTrue(wordValidator.isAllLetters("APPLE"));
-
     }
 
 
     @Test
     public void EmptyStringIsAllLetters(){
         assertFalse(wordValidator.isAllLetters(""));
-
     }
 
 
     @Test
     public void containsNumberIsAllLetters(){
         assertFalse(wordValidator.isAllLetters("a1ppl"));
-
     }
 
     @Test
     public void containsSymbolIsAllLetters(){
         assertFalse(wordValidator.isAllLetters("a@ppl"));
-
     }
 }
